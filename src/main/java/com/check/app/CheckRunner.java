8c8edab199383d514f8325b3be9164c223d13812
PluM2006 <PluM2006@tut.by>
2021-11-22 16:26:20 +0300

    continue to work

--- src/main/java/com/check/app/CheckRunner.java
+++ src/main/java/com/check/app/CheckRunner.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -21,10 +22,9 @@ public class CheckRunner {
 	public static void main(String[] args) {
 		 InputStream isProduct = Product.class.getResourceAsStream("/product.json");
 		 InputStream isCard = Card.class.getResourceAsStream("/card.json");
-		 List<Card> listCard = new ArrayList<Card>();
-		 List<CheckItem> listcheckitem = new ArrayList<CheckItem>();
+		 List<Card> listCardall = new ArrayList<Card>();
 		try {
-			listCard = new ObjectMapper().readValue(isCard, new TypeReference<List<Card>>(){});
+			listCardall = new ObjectMapper().readValue(isCard, new TypeReference<List<Card>>(){});
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -36,13 +36,25 @@ public class CheckRunner {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		 Arrays.asList(args).stream().filter(o -> o.matches("card(.*)")).findAny().orElse(null);
-		 
+//		 Arrays.asList(args).stream().filter(o -> o.matches("card(.*)")).findAny().orElse(null);
+		 List<CheckItem> listcheckitem = new ArrayList<CheckItem>();
+		 List<Card> listCard = new ArrayList<Card>();
+		 //Список продуктов чека
 		 for (int i = 0; i < args.length; i++) {
-			String[] test = args[i].split("-");
+			String[] a = args[i].split("-");
+			if (!a[0].contains("card")) {
+				Product prod = listProduct.stream().filter(p -> p.getId()==Integer.parseInt(a[0])).findAny().orElse(null);
+				CheckItem item = new CheckItem(prod, Integer.parseInt(a[1]), new BigDecimal(0), new BigDecimal(0));
+				listcheckitem.add(item);
+			} else {
+				Card ecard =listCardall.stream().filter(e -> e.getNumbercard().contains(a[1])).findAny().orElse(null);
+				listCard.add(ecard);
+			}
 			
 		}
-		 
+		 if (listCard.size()>1) {
+			 System.out.println("выберите нужную скидочную карту");
+		 }
 		 Product test = listProduct.stream().filter(prod -> "test".equals(prod.getName())).findAny().orElse(null);
 //		System.out.println(test.getName());
 		for (Card card : listCard) {
